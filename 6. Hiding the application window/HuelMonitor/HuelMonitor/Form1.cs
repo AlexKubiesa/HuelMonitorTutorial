1e4793494e8e1276b833fd83e0a4c6f7b5bff054
Alex Kubiesa <alex.kubiesa@outlook.com>
2022-01-16 16:20:31 +0000

    Add Step 6: Hiding the application window.

---
+++ 6. Hiding the application window/HuelMonitor/HuelMonitor/Form1.cs
@@ -0,0 +1,39 @@
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace HuelMonitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false;
+            ShowInTaskbar = false;
+
+            base.OnLoad(e);
+
+            bool available = AvailabilityChecker.IsChocolateHuelAvailable();
+
+            if (available)
+            {
+                new ToastContentBuilder()
+                    .AddText("Chocolate Huel available")
+                    .AddText("Chocolate Huel is available! Hooray!!!")
+                    .Show();
+            }
+            else
+            {
+                new ToastContentBuilder()
+                    .AddText("Chocolate Huel not available")
+                    .AddText("Sorry, chocolate Huel isn't available today.")
+                    .Show();
+            }
+
+            Close();
+        }
+    }
+}
