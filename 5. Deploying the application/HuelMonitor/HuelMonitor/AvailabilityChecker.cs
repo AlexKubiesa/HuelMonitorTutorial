215e50fbf78a172fb1bea0c29ed0fca92e061ea6
Alex Kubiesa <alex.kubiesa@outlook.com>
2022-01-16 16:16:14 +0000

    Add Step 5: Deploying the application.

---
+++ 5. Deploying the application/HuelMonitor/HuelMonitor/AvailabilityChecker.cs
@@ -0,0 +1,48 @@
+﻿using HtmlAgilityPack;
+using HuelMonitor.Data;
+using System.Text.Json;
+
+namespace HuelMonitor
+{
+    internal static class AvailabilityChecker
+    {
+        public static bool IsChocolateHuelAvailable()
+        {
+            Console.WriteLine("Checking availability of chocolate Huel...");
+
+            HtmlWeb web = new();
+            var doc = web.Load("https://uk.huel.com/products/huel-ready-to-drink");
+            var dataProductJsonNode = doc.DocumentNode.SelectSingleNode("//script[@data-product-json]");
+            string dataProductJson = dataProductJsonNode.InnerText;
+            var dataProducts = JsonSerializer.Deserialize<DataProducts>(dataProductJson);
+
+            if (dataProducts == null)
+            {
+                throw new ApplicationException("Unable to read Huel flavour data.");
+            }
+
+            var variants = dataProducts.variants;
+
+            var chocolate = variants.FirstOrDefault(x => x.title.Contains("Chocolate"));
+            if (chocolate == null)
+            {
+                throw new ApplicationException("Unable to find chocolate flavour.");
+            }
+
+            return IsAvailable(chocolate);
+        }
+
+        static bool IsAvailable(Variant variant)
+        {
+            switch (variant.available)
+            {
+                case "yes":
+                    return true;
+                case "subscription_only":
+                    return false;
+                default:
+                    throw new ApplicationException($"Unable to parse availability status '{variant.available}'.");
+            }
+        }
+    }
+}
